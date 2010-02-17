1b50d3d5a3d98b54b5f7d9453d9512b8088e2b39
ssalonen <ssalonen@gmail.com>
2010-02-17 19:34:47 +0000

    Issue 126 & Issue 281: Deleting obsolete classes. Implemented search-routine of external keyboards, should find the external Finnish keyboard now. External dictionary keyboards are still under work, getAllExternalDictionaries under ExternalDictionaryFactory is not yet implemented (but it's trivial since the code is almost similar to KeyboardBuildersFactory)
    @menny: is intent action and metadata name OK?
    
    Settings seems to crash for some reason.

---
+++ LanguagePacks/Finnish/src/com/menny/anysoftkeyboard/finnish/FinnishKeyboardBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.menny.anysoftkeyboard.finnish;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class FinnishKeyboardBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}

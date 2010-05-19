e7675428b7f5cf4ec3df90d993d94497687bfd10
ssalonen <ssalonen@gmail.com>
2010-05-19 09:22:23 +0000

    AnySoftKeyboardPlugin, contains interfaces for theme plugins

---
+++ AnySoftKeyboardPlugin/src/com/menny/android/anysoftkeyboard/theme/ThemePluginConstants.java
@@ -0,0 +1,19 @@
+package com.menny.android.anysoftkeyboard.theme;
+
+public class ThemePluginConstants {
+	 /**
+     * This is the interface name that a broadcast receiver implementing an
+     * theme plugin should say that it supports -- that is, this is
+     * the action it uses for its intent filter.
+     */
+    public static final String RECEIVER_INTERFACE = "com.menny.android.anysoftkeyboard.THEME";
+
+
+    /**
+     * The name of the provider.
+     */
+    public static final String METADATA_PROVIDER_NAME = "com.menny.android.anysoftkeyboard.themes.provider_full_name";
+
+    public static final String METADATA_PROVIDER_DESCRIPTION = "com.menny.android.anysoftkeyboard.themes.provider_description";
+    public static final String METADATA_PROVIDER_ID = "com.menny.android.anysoftkeyboard.themes.provider_id";
+}

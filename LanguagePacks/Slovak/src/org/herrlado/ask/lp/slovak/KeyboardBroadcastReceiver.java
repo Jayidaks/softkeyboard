b3d09be5114d4c796d041303014fb696ec687d50
herrlado <herrlado@8c604098-42ae-11de-b6cc-f32266f7ba4a>
2010-06-03 21:41:53 +0000

    AnySoftkeyboardLanguagePackSlovak renamed to Slovak

---
+++ LanguagePacks/Slovak/src/org/herrlado/ask/lp/slovak/KeyboardBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package org.herrlado.ask.lp.slovak;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyboardBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}

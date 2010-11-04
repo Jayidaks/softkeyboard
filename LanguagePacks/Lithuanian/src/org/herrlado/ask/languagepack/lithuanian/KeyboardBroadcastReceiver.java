036190aa43ff362a2e5ee7776c3f6a0736e580d7
herrlado <herrlado@gmail.com>
2010-11-04 14:42:51 +0000

    api usage

--- LanguagePacks/Lithuanian/src/org/herrlado/ask/languagepack/lithuanian/KeyboardBroadcastReceiver.java
+++ LanguagePacks/Lithuanian/src/org/herrlado/ask/languagepack/lithuanian/KeyboardBroadcastReceiver.java
@@ -1,14 +1,10 @@
 package org.herrlado.ask.languagepack.lithuanian;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
 
-public class KeyboardBroadcastReceiver extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(Context arg0, Intent arg1) {
 
-    }
+
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
 
 }

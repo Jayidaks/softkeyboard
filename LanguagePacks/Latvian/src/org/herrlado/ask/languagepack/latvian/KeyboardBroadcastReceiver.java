0db58557ffddeeec01ebb05246f7e72fa01e8bc5
herrlado <herrlado@gmail.com>
2010-11-25 21:40:10 +0000

    latvian prepared for new api

--- LanguagePacks/Latvian/src/org/herrlado/ask/languagepack/latvian/KeyboardBroadcastReceiver.java
+++ LanguagePacks/Latvian/src/org/herrlado/ask/languagepack/latvian/KeyboardBroadcastReceiver.java
@@ -1,15 +1,8 @@
 
 package org.herrlado.ask.languagepack.latvian;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
 
-public class KeyboardBroadcastReceiver extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context arg0, Intent arg1) {
-
-    }
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
 
 }

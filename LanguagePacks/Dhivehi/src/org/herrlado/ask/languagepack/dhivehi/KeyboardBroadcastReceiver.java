6fbef0f2962c322200ceb3a9a041ce0e07bdc5f9
herrlado <herrlado@8c604098-42ae-11de-b6cc-f32266f7ba4a>
2011-03-13 13:31:17 +0000

    initia Dhivehi

---
+++ LanguagePacks/Dhivehi/src/org/herrlado/ask/languagepack/dhivehi/KeyboardBroadcastReceiver.java
@@ -0,0 +1,8 @@
+package org.herrlado.ask.languagepack.dhivehi;
+
+import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
+
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
+
+
+}

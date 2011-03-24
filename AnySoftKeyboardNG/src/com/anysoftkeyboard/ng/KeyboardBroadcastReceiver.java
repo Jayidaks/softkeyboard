63d769305ed65856bec78f2e05268ac92ae419e3
mennyed <mennyed@gmail.com>
2011-03-24 10:51:24 +0000

    Removed anysoftkeyboard/ng - there is no need for this project.

--- AnySoftKeyboardNG/src/com/anysoftkeyboard/ng/KeyboardBroadcastReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.anysoftkeyboard.ng;
-
-import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
-
-public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
-
-}

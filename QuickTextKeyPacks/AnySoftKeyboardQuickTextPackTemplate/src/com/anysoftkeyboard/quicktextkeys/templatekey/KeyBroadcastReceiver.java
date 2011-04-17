b3abef6b58c5531609287259af6f9afd47cea3ce
mennyed <mennyed@gmail.com>
2011-04-17 07:33:57 +0000

    template projects into the SVN (so we can branch them)

---
+++ QuickTextKeyPacks/AnySoftKeyboardQuickTextPackTemplate/src/com/anysoftkeyboard/quicktextkeys/templatekey/KeyBroadcastReceiver.java
@@ -0,0 +1,10 @@
+package com.anysoftkeyboard.quicktextkeys.templatekey;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {}
+}

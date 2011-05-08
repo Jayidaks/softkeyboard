1d93905cf3dc71f7eabcbea4813dc26544ac0003
mennyed <mennyed@gmail.com>
2011-05-08 11:28:02 +0000

    A fix release for 20110508

--- DictionaryTools/DictionaryCreator/src/com/anysoftkeyboard/dictionarycreator/UI.java
+++
@@ -1,8 +0,0 @@
-package com.anysoftkeyboard.dictionarycreator;
-
-public interface UI {
-	void showErrorMessage(String message);
-	void updateProgressState(String message, int precentage);
-	
-	void onEnded();
-}

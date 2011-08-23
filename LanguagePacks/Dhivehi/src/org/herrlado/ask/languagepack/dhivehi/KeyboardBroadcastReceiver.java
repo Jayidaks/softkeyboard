59abf4c423e94113d971478ffbefa234801c73ae
mennyed <mennyed@gmail.com>
2011-08-23 07:40:14 +0000

    Market release 20110815 - 47

--- LanguagePacks/Dhivehi/src/org/herrlado/ask/languagepack/dhivehi/KeyboardBroadcastReceiver.java
+++
@@ -1,8 +0,0 @@
-package org.herrlado.ask.languagepack.dhivehi;
-
-import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
-
-public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
-
-
-}

14e7f2977b1afb157e4f3320f448068efea858b4
mennyed <mennyed@gmail.com>
2011-09-08 06:20:25 +0000

    ASK Release 20110907 - 49

--- LanguagePacks/Czech/src/org/herrlado/ask/languagepack/czech/KeyboardBroadcastReceiver.java
+++
@@ -1,6 +0,0 @@
-package org.herrlado.ask.languagepack.czech;
-
-import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
-public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
-
-}
--- LanguagePacks/Italian/src/org/herrlado/ask/languagepack/italian/KeyboardBroadcastReceiver.java
+++
@@ -1,7 +0,0 @@
-package org.herrlado.ask.languagepack.italian;
-
-import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
-
-public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
-
-}

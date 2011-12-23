49036ada7c559605a29403bf0b4d2077ca2794cf
mennyed <mennyed@gmail.com>
2011-12-23 20:56:30 +0000

    Release 61

--- LanguagePacks/Latvian/src/org/herrlado/ask/languagepack/latvian/KeyboardBroadcastReceiver.java
+++
@@ -1,9 +0,0 @@
-
-package org.herrlado.ask.languagepack.latvian;
-
-import com.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
-
-
-public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
-
-}
--- LanguagePacks/Lithuanian/src/org/herrlado/ask/languagepack/lithuanian/KeyboardBroadcastReceiver.java
+++
@@ -1,8 +0,0 @@
-package org.herrlado.ask.languagepack.lithuanian;
-
-import com.anysoftkeyboard.api.LanguageChangeddBroadcastReceiver;
-
-public class KeyboardBroadcastReceiver extends
-		LanguageChangeddBroadcastReceiver {
-
-}

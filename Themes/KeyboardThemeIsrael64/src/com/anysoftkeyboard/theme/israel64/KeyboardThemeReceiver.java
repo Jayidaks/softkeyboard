5a87ae4764788032d36892d16842eca30fb8c99c
mennyed <mennyed@gmail.com>
2012-04-23 08:14:15 +0000

    Israel 64 Bday Theme

---
+++ Themes/KeyboardThemeIsrael64/src/com/anysoftkeyboard/theme/israel64/KeyboardThemeReceiver.java
@@ -0,0 +1,14 @@
+package com.anysoftkeyboard.theme.israel64;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyboardThemeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
